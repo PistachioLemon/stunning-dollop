a9ee29fba3b0bdec9c1461bc059e7d02f4278ed0
PistachioLemon <79180456+PistachioLemon@users.noreply.github.com>
2026-08-09 22:15:45 -0700

    Export Repair Librarian from healing package

--- nova/healing/__init__.py
+++ nova/healing/__init__.py
@@ -1,9 +1,12 @@
 from .engine import SelfHealingEngine
+from .librarian import RepairKnowledge, RepairLibrarian
 from .memory import RepairMemory
 from .models import HealthFinding, RepairHandler, RepairRecipe, RiskLevel
 
 __all__ = [
     "SelfHealingEngine",
+    "RepairLibrarian",
+    "RepairKnowledge",
     "RepairMemory",
     "HealthFinding",
     "RepairHandler",
