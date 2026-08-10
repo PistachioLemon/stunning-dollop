08cefd853375e552d7199874613e6041a3ed04c5
PistachioLemon <79180456+PistachioLemon@users.noreply.github.com>
2026-08-09 22:19:37 -0700

    Export Nova healing evidence adapters

--- nova/healing/__init__.py
+++ nova/healing/__init__.py
@@ -1,13 +1,29 @@
 from .engine import SelfHealingEngine
+from .integrations import (
+    KnowledgeSource,
+    RepairEvidenceLoader,
+    local_llm_probe,
+    log_signature_probe,
+    protected_sandbox_probe,
+    register_default_evidence,
+)
 from .librarian import RepairKnowledge, RepairLibrarian
 from .memory import RepairMemory
 from .models import HealthFinding, RepairHandler, RepairRecipe, RiskLevel
+from .runtime import HealingRuntime
 
 __all__ = [
     "SelfHealingEngine",
+    "HealingRuntime",
     "RepairLibrarian",
     "RepairKnowledge",
     "RepairMemory",
+    "KnowledgeSource",
+    "RepairEvidenceLoader",
+    "local_llm_probe",
+    "log_signature_probe",
+    "protected_sandbox_probe",
+    "register_default_evidence",
     "HealthFinding",
     "RepairHandler",
     "RepairRecipe",
