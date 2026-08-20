4d027f326c525e7eef1caf06b8b704f1225c5715
PistachioLemon <79180456+PistachioLemon@users.noreply.github.com>
2026-08-20 10:59:58 -0700

    Export routing regression evaluator

---
+++ nova/routing/__init__.py
@@ -0,0 +1,3 @@
+from .regression import RouteExpectation, RouteObservation, RouteRegressionResult, evaluate_route, evaluate_suite
+
+__all__ = ["RouteExpectation", "RouteObservation", "RouteRegressionResult", "evaluate_route", "evaluate_suite"]
